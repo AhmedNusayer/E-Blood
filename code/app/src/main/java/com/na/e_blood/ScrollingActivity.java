ae5d41b6865ec2493c7f1d1367214fda919c903f
65-Niloy <saifulislam65@iut-dhaka.edu>
2019-03-30 02:32:55 +0600

    #Basic UI Design 1 *Splash Screen *Homepage *Search, Setting, About empty activity.

--- code/app/src/main/java/com/na/e_blood/ScrollingActivity.java
+++ code/app/src/main/java/com/na/e_blood/ScrollingActivity.java
@@ -1,51 +1,68 @@
 package com.na.e_blood;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class ScrollingActivity extends AppCompatActivity {
+public class ScrollingActivity extends AppCompatActivity implements View.OnClickListener{
+    CardView card_search;
+    CardView card_information;
+    CardView card_setting;
+    CardView card_about;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling);
+        /*new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(ScrollingActivity.this, SplashActivity.class);
+                startActivity(intent);
+                finish();
+            },SPLASH_TIME_OUT);
+        });*/
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        cardViewInitializer();
+
+    }
+    public void cardViewInitializer(){
+        card_search = (CardView) findViewById(R.id.card_search);
+        card_search.setOnClickListener(this);
+        card_information = (CardView) findViewById(R.id.card_information);
+        card_information.setOnClickListener(this);
+        card_setting = (CardView) findViewById(R.id.card_setting);
+        card_setting.setOnClickListener(this);
+        card_about = (CardView) findViewById(R.id.card_about);
+        card_about.setOnClickListener(this);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_scrolling, menu);
-        return true;
-    }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onClick(View v) {
+        if(v.getId()== R.id.card_search){
+            Intent intent = new Intent(ScrollingActivity.this, SearchActivity.class);
+            startActivity(intent);
+        }else if(v.getId()== R.id.card_information){
+            Intent intent = new Intent(ScrollingActivity.this, InformationActivity.class);
+            startActivity(intent);
+        }else if(v.getId()== R.id.card_setting){
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        }else if(v.getId()== R.id.card_about){
+            Intent intent = new Intent(ScrollingActivity.this, AboutActivity.class);
+            startActivity(intent);
         }
-        return super.onOptionsItemSelected(item);
+
     }
 }
+
